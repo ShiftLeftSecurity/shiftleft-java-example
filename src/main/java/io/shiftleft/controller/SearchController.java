8bc833ff209802aaafe559538f3e62a02dd83178
Prabhu Subramanian <prabhu@shiftleft.io>
2020-10-15 20:26:10 +0100

    Address rce issues

--- src/main/java/io/shiftleft/controller/SearchController.java
+++ src/main/java/io/shiftleft/controller/SearchController.java
@@ -1,15 +1,19 @@
 package io.shiftleft.controller;
 
+import io.shiftleft.model.Customer;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
+import org.springframework.binding.expression.Expression;
+import org.springframework.binding.expression.ParserContext;
+import org.springframework.binding.expression.beanwrapper.BeanWrapperExpressionParser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import org.springframework.binding.expression.support.FluentParserContext;
+import org.springframework.binding.expression.support.StaticExpression;
+import org.springframework.binding.mapping.MappingResult;
 
 /**
  * Search login
@@ -21,9 +25,10 @@ public class SearchController {
   public String doGetSearch(@RequestParam String foo, HttpServletResponse response, HttpServletRequest request) {
     java.lang.Object message = new Object();
     try {
-      ExpressionParser parser = new SpelExpressionParser();
-      Expression exp = parser.parseExpression(foo);
-      message = (Object) exp.getValue();
+      ParserContext parserContext = new FluentParserContext().evaluate(Customer.class);
+      BeanWrapperExpressionParser parser = new BeanWrapperExpressionParser();
+      Expression exp = parser.parseExpression(foo, parserContext);
+      message = (Object) exp.getValueType(new Customer());
     } catch (Exception ex) {
       System.out.println(ex.getMessage());
     }
